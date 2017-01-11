8f14201ee3485e16b16b588a79ced00b3090ef38
tobuer <tobuer@tobuerdeMacBook-Pro.local>
2017-01-11 19:33:00 +0800

    Initial Commit

---
+++ TimeAccount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeAccount
+//
+//  Created by tobuer on 2017/1/11.
+//  Copyright © 2017年 tobuer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
